266b8206cb20775566965725e74442091d194aec
yuliyaBir <zhivo4ka@gmail.com>
2023-08-23 07:46:01 +0200

    Initial commit

---
+++ src/main/java/be/vdab/fietsen/FietsenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.fietsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FietsenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FietsenApplication.class, args);
+    }
+
+}
